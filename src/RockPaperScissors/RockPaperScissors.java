b45f78e5ba216ed448332b19458f829fe993dcda
kshenoy25 <54074703+kshenoy25@users.noreply.github.com>
2024-08-23 21:16:03 -0500

    RockPaperScissors

---
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String[] rps = {"r", "p", "s"};
+
+            // want to get a random index using Random
+            String computerMove = rps[new Random().nextInt(rps.length)];
+            String playerMove;
+
+
+            while (true) {
+                System.out.println("Please enter your move (r, p, or s)");
+                playerMove = scanner.nextLine();
+                if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
+                    break;
+                }
+                // player did not give a valid input/move
+                System.out.println(playerMove + "is not a valid move.");
+            }
+
+            System.out.println("Computer played: " + computerMove);
+            // tie
+            if (playerMove.equals(computerMove)) {
+                System.out.println("The gane was a tie!");
+            } else if (playerMove.equals("r")) {
+                if (computerMove.equals("p")) {
+                    System.out.println("You lose!");
+                } else if (computerMove.equals("s")) {
+                    System.out.println("You win!");
+                }
+            } else if (playerMove.equals("p")) {
+                if (computerMove.equals("r")) {
+                    System.out.println("You win!");
+                } else if (computerMove.equals("s")) {
+                    System.out.println("You lose!");
+                }
+            } else if (playerMove.equals("s")) {
+                if (computerMove.equals("p")) {
+                    System.out.println("You win!");
+                } else if (computerMove.equals("r")) {
+                    System.out.println("You lose!");
+                }
+            }
+
+            System.out.println("Play again? (y/n)");
+            String playAgain = scanner.nextLine();
+
+            if (!playAgain.equals("y")) {
+                break;
+            }
+        }
+    }
+}
